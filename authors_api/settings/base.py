d3c0c48f9119bcbe212a2a354572b556fd278789
Goodnessmbakara <amicablembakara50@gmail.com>
2024-01-23 00:48:17 +0100

    chore: setup Makefile chore

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -74,7 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.i18n',
-                'django.template.context_processors.static'
+                'django.template.context_processors.static',
                 'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
             ],
